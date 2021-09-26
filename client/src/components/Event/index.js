96c3f6163ced30ca4e758503beb7fa55eaf7a6f6
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:51:05 -0300

    Adicionado uma linha no evento

--- client/src/components/Event/index.js
+++ client/src/components/Event/index.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { TableCell, TableRow } from "semantic-ui-react";
 
-function Event({ event }) {
-  return <div>Event</div>;
+function Event({
+  event: {
+    title = "",
+    description = "",
+    begin = new Date(),
+    finish = new Date(),
+    owner,
+    guests = [],
+  },
+}) {
+  return (
+    <TableRow>
+      <TableCell>
+        {begin.toLocaleDateString("pt-br") + finish.toLocaleDateString("pt-br")}
+      </TableCell>
+      <TableCell>{title}</TableCell>
+      <TableCell>{description}</TableCell>
+    </TableRow>
+  );
 }
 
 export default Event;
