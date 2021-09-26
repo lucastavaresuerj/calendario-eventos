4ea25ff963b8a01d8a9d178972c2f222d341aa5c
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:37:05 -0300

    Consertando getItem do token

--- client/src/api/calendar.js
+++ client/src/api/calendar.js
@@ -5,7 +5,7 @@ const { REACT_APP_CALENDAR_API_ADDRESS } = process.env;
 const calendar = axios.create({
   baseURL: REACT_APP_CALENDAR_API_ADDRESS,
   headers: {
-    "x-access-token": sessionStorage.getItem("x-access-token"),
+    "x-access-token": sessionStorage.getItem("token"),
     owner: sessionStorage.getItem("owner"),
   },
 });
