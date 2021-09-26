25f1d8498cd051b216861cfa2140f9ecf77bc287
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:56:07 -0300

    Adicionando o componente do calendário local

---
+++ client/src/components/LocaleCalendar/index.js
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { Calendar } from "react-multi-date-picker";
+
+import * as locales from "./locales";
+import "./style.scss";
+
+function LocaleCalendar({
+  locale = "pt-BR",
+  showMonth = "MMM",
+  showWeek = "W",
+  ...calendarProps
+}) {
+  const [localeAttr, setLocaleAttr] = useState(getLocale());
+  const [month, setMonth] = useState();
+  const [weekDays, setWeekDays] = useState();
+
+  function getLocale() {
+    return Object.values(locales).find((loc) => {
+      return loc?.name == locale;
+    });
+  }
+
+  useEffect(() => {
+    if (localeAttr) {
+      setMonth(localeAttr.months.map((month) => month[3 - showMonth.length]));
+      setWeekDays(localeAttr.weekDays.map((week) => week[3 - showWeek.length]));
+    }
+  }, [localeAttr]);
+
+  return (
+    <Calendar
+      locale={localeAttr}
+      months={month}
+      weekDays={weekDays}
+      showOtherDays
+      shadow={false}
+      {...calendarProps}
+    />
+  );
+}
+
+export default LocaleCalendar;
---
+++ client/src/components/LocaleCalendar/locales/ptBR.js
@@ -0,0 +1,35 @@
+const ptBR = {
+  name: "pt-BR",
+  months: [
+    ["Janeiro", "Jan", "J"],
+    ["Fevereiro", "Fev", "F"],
+    ["Março", "Mar", "M"],
+    ["Abril", "Abr", "A"],
+    ["Maio", "Mai", "M"],
+    ["Junho", "Jun", "J"],
+    ["Julho", "Jul", "J"],
+    ["Agosto", "Ago", "A"],
+    ["Setembro", "Set", "S"],
+    ["Outubro", "Out", "O"],
+    ["Novembro", "Nov", "N"],
+    ["Dezembro", "Dez", "D"],
+  ],
+  weekDays: [
+    ["Domingo", "Dom", "D"],
+    ["Segunda", "Seg", "S"],
+    ["Terça", "Ter", "T"],
+    ["Quarta", "Qua", "Q"],
+    ["Quinta", "Qui", "Q"],
+    ["Sexta", "Sex", "S"],
+    ["Sábado", "Sáb", "S"],
+    ["Domingo", "Dom", "D"],
+  ],
+  digits: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"],
+  meridiems: [
+    ["AM", "am"],
+    ["PM", "pm"],
+  ],
+  format: "DD/MM/YYYY - HH:mm:ss",
+};
+
+export default ptBR;
