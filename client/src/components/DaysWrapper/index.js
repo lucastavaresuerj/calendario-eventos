5ab7751c5d9925a9fe7f45d9df1822bde29e5bdb
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:48:32 -0300

    Colocando Grid no componente envolta do DaysWrapper

--- client/src/components/DaysWrapper/index.js
+++ client/src/components/DaysWrapper/index.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { Grid } from "semantic-ui-react";
 
 import { Day } from "../";
 
-function DaysWrapper({ days }) {
-  return days.map((events) => <Day event={events} />);
+function DaysWrapper({ days = [] }) {
+  return (
+    <Grid className="days-wrapper">
+      {days.map((events) => {
+        console.log(events);
+        return <Day className="days" events={events} />;
+      })}
+    </Grid>
+  );
 }
 
 export default DaysWrapper;
