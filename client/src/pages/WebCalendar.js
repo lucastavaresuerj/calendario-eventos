ba156a4ff11ef7c80f56571cc93a6cd0cc6421e6
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:58:41 -0300

    Adicionando a grid da página principal

--- client/src/pages/WebCalendar.js
+++ client/src/pages/WebCalendar.js
@@ -1,14 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Grid } from "semantic-ui-react";
+import { DateObject } from "react-multi-date-picker";
 
-import { Page, Header } from "../components";
+import calendar from "src/api/calendar";
+import { Page, Header, DaysWrapper, Month } from "src/components";
 
 function WebCalendar() {
-  const [day, setDay] = useState(new Date());
+  const [day, setDay] = useState(new DateObject());
+  const [days, setDays] = useState([]);
+
+  useEffect(() => {
+    getDays();
+  }, []);
+
+  async function getDays() {
+    try {
+      const { data } = await calendar.get("/day");
+      console.log(data);
+      setDays(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   return (
-    <Page title="Calendario" restricted>
+    <div>
+      <Page title="Calendario" />
       <Header />
-    </Page>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width="3">
+            <Month />
+          </Grid.Column>
+          <Grid.Column width="13">
+            <DaysWrapper days={days} />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
   );
 }
 
