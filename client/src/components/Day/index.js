1443aa714b905cc186af8d62033cdb3b7c0ad929
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-26 11:46:49 -0300

    Colocando a Grid em Dia

--- client/src/components/Day/index.js
+++ client/src/components/Day/index.js
@@ -1,8 +1,17 @@
 import React from "react";
-import { TableRow } from "semantic-ui-react";
+import { Grid } from "semantic-ui-react";
 
-function Day({ event }) {
-  return <TableRow>Day</TableRow>;
+import { EventsWrapper } from "../";
+
+function Day({ events, day = new Date() }) {
+  return (
+    <Grid.Row className="days-grid-row">
+      <Grid.Column>{day.toLocaleDateString("pt-br")}</Grid.Column>
+      <Grid.Column>
+        <EventsWrapper events={events} />
+      </Grid.Column>
+    </Grid.Row>
+  );
 }
 
 export default Day;
