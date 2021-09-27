d71c380f409ff39c51003deea6157f808e671425
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-27 11:57:00 -0300

    Passando as credenciáis de usuário para o localStorage

--- client/src/api/calendar.js
+++ client/src/api/calendar.js
@@ -5,8 +5,8 @@ const { REACT_APP_CALENDAR_API_ADDRESS } = process.env;
 const calendar = axios.create({
   baseURL: REACT_APP_CALENDAR_API_ADDRESS,
   headers: {
-    "x-access-token": sessionStorage.getItem("token"),
-    owner: sessionStorage.getItem("owner"),
+    "x-access-token": localStorage.getItem("token"),
+    owner: localStorage.getItem("owner"),
   },
 });
 
