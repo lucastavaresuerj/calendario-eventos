8864d1437c1aaf81368238f4a7ceb20974807c5a
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-27 19:26:58 -0300

    Setando o dia atraves de outros componentes

--- client/src/pages/WebCalendar/index.js
+++ client/src/pages/WebCalendar/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Grid } from "semantic-ui-react";
 import { DateObject } from "react-multi-date-picker";
 
+import "./style.scss";
 import calendar from "src/api/calendar";
 import { Page, Header, DaysWrapper, Month } from "src/components";
 
@@ -13,9 +14,18 @@ function WebCalendar() {
     getDays();
   }, []);
 
-  async function getDays() {
+  function setNewDate(date = new Date()) {
+    setDay(new DateObject(date));
+    getDays(date instanceof DateObject ? date.toDate() : date);
+  }
+
+  async function getDays(beginDay = new Date()) {
     try {
-      const { data } = await calendar.get("/day");
+      const { data } = await calendar.get("/day", {
+        params: {
+          beginDay,
+        },
+      });
       console.log(data);
       setDays(data);
     } catch (error) {
@@ -25,11 +35,11 @@ function WebCalendar() {
 
   return (
     <Page title="Calendario">
-      <Header />
-      <Grid>
+      <Header setDay={setNewDate} />
+      <Grid celled className="main">
         <Grid.Row>
           <Grid.Column width="3">
-            <Month />
+            <Month setDay={setNewDate} day={day} />
           </Grid.Column>
           <Grid.Column width="13">
             <DaysWrapper days={days} />
