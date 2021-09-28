3e301f0aeb78052b65dc9cd37bca295d6ccb2ba9
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-28 20:41:25 -0300

    Adicionando middleware para renovar o token a cada requisição

--- client/src/api/calendar.js
+++ client/src/api/calendar.js
@@ -5,9 +5,23 @@ const { REACT_APP_CALENDAR_API_ADDRESS } = process.env;
 const calendar = axios.create({
   baseURL: REACT_APP_CALENDAR_API_ADDRESS,
   headers: {
-    "x-access-token": localStorage.getItem("token"),
     owner: localStorage.getItem("owner"),
   },
 });
+
+calendar.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers["x-access-token"] = token;
+    } else {
+      delete calendar.defaults.headers.common["x-access-token"];
+    }
+    return config;
+  },
+
+  (error) => Promise.reject(error)
+);
 
 export default calendar;
