ab33929a96d972eb7b9ab60acce37be228224f47
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-28 20:42:24 -0300

    Passando as props de buttons para o esse componente

---
+++ client/src/components/CreateButton/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Button } from "semantic-ui-react";
+
+function CreateButton(props) {
+  return <Button {...props}>Adicionar Evento</Button>;
+}
+
+export default CreateButton;
