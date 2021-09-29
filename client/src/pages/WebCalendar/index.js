f4c6b53940dbe2121fddb4b3ab30766b58ef9ce6
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-29 18:39:12 -0300

    Adicinando a logica de contexto de eventos

--- client/src/pages/WebCalendar/index.js
+++ client/src/pages/WebCalendar/index.js
@@ -1,9 +1,10 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Grid } from "semantic-ui-react";
 import { DateObject } from "react-multi-date-picker";
 
 import "./style.scss";
 import calendar from "src/api/calendar";
+import { EventContext } from "src/context/Event";
 import {
   Page,
   Header,
@@ -17,14 +18,31 @@ function WebCalendar() {
   const [day, setDay] = useState(new DateObject());
   const [days, setDays] = useState([]);
   const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState(false);
+
+  const eventContext = useContext(EventContext);
+
+  useEffect(() => {
+    if (!eventContext.isEmpty() && eventContext.getEventOption() === "edit") {
+      setOpen(true);
+      setEdit(true);
+    }
+  }, [eventContext]);
+
+  useEffect(() => {
+    if (!open) {
+      setEdit(false);
+    }
+  }, [open]);
 
   useEffect(() => {
     getDays();
   }, []);
 
   function setNewDate(date = new Date()) {
-    setDay(new DateObject(date));
-    getDays(date instanceof DateObject ? date.toDate() : date);
+    const dateObject = new DateObject(date);
+    setDay(dateObject);
+    getDays(dateObject.toDate());
   }
 
   async function getDays(beginDay = new Date()) {
@@ -40,21 +58,46 @@ function WebCalendar() {
     }
   }
 
+  async function onSubmit(eventFields) {
+    try {
+      if (edit) {
+        await calendar.put(
+          `/event/${eventContext.getEvent()["_id"]}`,
+          eventFields
+        );
+      } else {
+        await calendar.post("/event", eventFields);
+      }
+
+      getDays(day.toDate());
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
   return (
     <Page title="Calendario">
-      <Header setDay={setNewDate} />
+      <Header setDay={setNewDate} day={day} />
       <Grid celled className="main">
-        <Grid.Row>
+        <Grid.Row centered>
           <Grid.Column className="left-column" width="3">
             <CreateButton onClick={() => setOpen(!open)} />
             <Month setDay={setNewDate} day={day} />
           </Grid.Column>
           <Grid.Column className="right-column" width="13">
-            <DaysWrapper days={days} />
+            <DaysWrapper days={days} reloadDays={getDays} setDay={setNewDate} />
           </Grid.Column>
         </Grid.Row>
       </Grid>
-      <CreateEventForm date={day} open={open} setOpen={setOpen} />
+      {open && (
+        <CreateEventForm
+          date={day}
+          open={open}
+          edit={edit}
+          setOpen={setOpen}
+          onSubmit={onSubmit}
+        />
+      )}
     </Page>
   );
 }
