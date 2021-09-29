7a65cc81e106d9e10a99e8b3ab278343030cb50d
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-29 18:30:44 -0300

    Mudando de dias pelas setas do header

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -2,15 +2,27 @@ import React from "react";
 import { Grid } from "semantic-ui-react";
 
 import "./style.scss";
-import { TodayButton, NavDaysButton } from "../";
+import { TodayButton, NavDaysButton, UserMenu } from "../";
+import { DateObject } from "react-multi-date-picker";
 
-function Header({ setDay }) {
+function Header({ setDay, day }) {
   return (
     <Grid className="header-app">
       <Grid.Row>
-        <TodayButton {...{ setDay }} />
-        <NavDaysButton previous />
-        <NavDaysButton next />
+        <Grid.Column floated="right" width="13">
+          <TodayButton {...{ setDay }} />
+          <NavDaysButton
+            previous
+            onClick={() => setDay(new DateObject(day).subtract(1, "day"))}
+          />
+          <NavDaysButton
+            next
+            onClick={() => setDay(new DateObject(day).add(1, "day"))}
+          />
+        </Grid.Column>
+        <Grid.Column width="2">
+          <UserMenu />
+        </Grid.Column>
       </Grid.Row>
     </Grid>
   );
--- client/src/components/NavDaysButton/index.js
+++ client/src/components/NavDaysButton/index.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Icon } from "semantic-ui-react";
 
-function NavDaysButton({ next = false, previous = false }) {
-  const direction = next ? "next" : previous ? "previous" : "unknow";
+function NavDaysButton({ next = false, previous = false, ...props }) {
+  const direction = next ? "angle right" : previous ? "angle left" : "unknow";
 
-  return <div>{direction}</div>;
+  return <Icon link name={direction} size="big" {...props} />;
 }
 
 export default NavDaysButton;
