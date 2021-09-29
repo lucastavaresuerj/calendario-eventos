dca54e419b62a54130ce401d97adea16178ffed6
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-29 18:26:46 -0300

    Clicando no dia faz o dia da aplicação virar o dia clicado

--- client/src/components/Day/index.js
+++ client/src/components/Day/index.js
@@ -3,11 +3,11 @@ import { Grid } from "semantic-ui-react";
 
 import { EventsWrapper, DayHeader } from "../";
 
-function Day({ day = new Date(), events = [] }) {
+function Day({ day = new Date(), events = [], setDay }) {
   return (
     <Grid.Row className="days-grid-row">
       <Grid.Column width={2}>
-        <DayHeader day={day} />
+        <DayHeader day={day} setDay={setDay} />
       </Grid.Column>
       <Grid.Column width={14}>
         <EventsWrapper events={events} />
