c1e8b04c8e46518383b248ca117b065b95aa8913
Lucas Tavares <lucastavares@localhost.localdomain>
2021-09-29 18:29:23 -0300

    Adicionando icones para opções de editar e deletar evento

--- client/src/components/Event/index.js
+++ client/src/components/Event/index.js
@@ -1,22 +1,15 @@
-import React, { useState } from "react";
-import { TableCell, TableRow } from "semantic-ui-react";
+import React, { useState, useContext } from "react";
+import { TableCell, TableRow, Icon } from "semantic-ui-react";
 
 import "./style.scss";
+import { EventContext } from "src/context/Event";
 
 function Event({
-  event: {
-    title = "",
-    description = "",
-    begin = "",
-    finish = "",
-    owner,
-    guests = [],
-  },
+  event: { title = "", description = "", begin = "", finish = "" },
   event,
 }) {
-  const [dateFormated, setDateFormated] = useState(formatDate());
-
-  console.log(event);
+  const [dateFormated] = useState(formatDate());
+  const eventContext = useContext(EventContext);
 
   function formatDate() {
     const options = { hour12: true, hour: "numeric", minute: "2-digit" };
@@ -29,18 +22,30 @@ function Event({
       .toLowerCase()
       .replace(/\s(.m)/gi, "$1")
       .replace(/(.*?)(.m) — (.*?)(.m)/gi, (math, p1, p2, p3, p4) => {
-        if (p2 == p4) {
+        if (p2 === p4) {
           return `${p1} — ${p3}${p2}`;
         }
         return math;
       });
   }
 
+  function setContextEvent(e, { name }) {
+    eventContext.setEvent({
+      ...event,
+      date: new Date(begin).setHours(0, 0, 0),
+      option: name === "edit" ? "edit" : "delete",
+    });
+  }
+
   return (
     <TableRow className="event-row">
-      <TableCell>{dateFormated}</TableCell>
-      <TableCell>{title}</TableCell>
-      <TableCell>{description}</TableCell>
+      <TableCell width="1">
+        <Icon link name="edit" onClick={setContextEvent} />
+        <Icon link name="trash" onClick={setContextEvent} />
+      </TableCell>
+      <TableCell width="2">{dateFormated}</TableCell>
+      <TableCell width="4">{title}</TableCell>
+      <TableCell className="description">{description}</TableCell>
     </TableRow>
   );
 }
